4d45afa4a0875125fda13467d714a9890b344c29
Hamed Pourkhaleghi <hamedp77@gmail.com>
2023-03-15 19:16:46 +0330

    check flags to be exactly 'false' otherwise will be evaluated to True

--- api.py
+++ api.py
@@ -13,14 +13,14 @@ def default():
     pw = []
     length = request.args.get('length', type=int, default=8)
     count = request.args.get('count', type=int, default=1)
-    use_digits = True if request.args.get(
-        'use_digits', default='true').lower() == 'true' else False
-    use_symbols = True if request.args.get(
-        'use_symbols', default='true').lower() == 'true' else False
-    use_uppercase = True if request.args.get(
-        'use_uppercase', default='true').lower() == 'true' else False
-    use_lowercase = True if request.args.get(
-        'use_lowercase', default='true').lower() == 'true' else False
+    use_digits = False if request.args.get(
+        'use_digits', default='true').lower() == 'false' else True
+    use_symbols = False if request.args.get(
+        'use_symbols', default='true').lower() == 'false' else True
+    use_uppercase = False if request.args.get(
+        'use_uppercase', default='true').lower() == 'false' else True
+    use_lowercase = False if request.args.get(
+        'use_lowercase', default='true').lower() == 'false' else True
 
     for _ in range(count):
         pw.append(generate_password(length, use_digits,
