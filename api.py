bf41f3bfc4f63fab30d6b6d5be3da433af193bb4
Hamed Pourkhaleghi <hamedp77@gmail.com>
2023-03-19 13:02:10 +0330

    add swagger ui

--- api.py
+++ api.py
@@ -1,9 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from generator import generate_password
 
 app = Flask(__name__)
 app.json.mimetype = 'application/json; charset=UTF-8'
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/openapi.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    SWAGGER_URL,
+    API_URL,
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 
 @app.route('/api/alpha/generate')
